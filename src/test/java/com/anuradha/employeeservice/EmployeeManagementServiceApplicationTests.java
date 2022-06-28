ced5f6caf00193ffd5a63696baafaa272dfd857c
anuradha <anuradha.kadurugasyaya@villvay.com>
2022-06-28 21:25:57 +0530

    first commit

---
+++ src/test/java/com/anuradha/employeeservice/EmployeeManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuradha.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
