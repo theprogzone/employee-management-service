6aa19255d3575e8ff937c8006ddb5d252da29c31
anuradha <theprogzone@gmail.com>
2022-07-05 23:46:06 +0530

    Allowed cross-origin for react front end

--- src/main/java/com/anuradha/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/anuradha/employeeservice/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/employee")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
